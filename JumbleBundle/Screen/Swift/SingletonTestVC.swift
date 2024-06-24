fe179f90555c7a3d28666ba0eb2601a8db0cdbf5
Madman-dev <dongjunlee222@gmail.com>
2024-06-24 12:15:17 +0900

    [FEAT]: ✨ singleton vs Static class testing

--- JumbleBundle/Screen/Swift/SingletonTestVC.swift
+++ JumbleBundle/Screen/Swift/SingletonTestVC.swift
@@ -31,11 +31,18 @@ class SingletonTestVC: UIViewController {
         // 가설 실패! > 이 이유를 생각해보자
 //         Singleton.shared.createToast(superview: view, withMessage: "이런 메시지")
         
-        // 아래 코드는 매번 호출이 된다.
+        // 아래 코드는 매번 호출 됨
 //        ToastMessenger.showToast(message: "이런 메시지가 나와요", in: self)
+        
+        // 아래 코드는 호출 안됨
+//        ToastTest.showMessage(message: "새로운 메시지")
+        
+        // on top of window?
+        Toast().createToast(message: "이런 메시지")
     }
 }
 
+// MARK: - Singleton
 // 싱글톤은 concurrent한 상황에서 발생할 수 있는 문제점이 있다.
 // concurrency를 해결하는 방법으로 대체 방안이 필요해보인다
 //class Singleton {
@@ -90,53 +97,68 @@ class SingletonTestVC: UIViewController {
 //    }
 //}
 
-//class ToastMessenger {
-//    // 이 친구는 잘 된다
-//    static func showToast(message: String, in viewController: UIViewController) {
-//            DispatchQueue.main.async {
-//                let toastLabel = UILabel()
-//                toastLabel.text = message
-//                toastLabel.textAlignment = .center
-//                toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-//                toastLabel.textColor = UIColor.white
-//                toastLabel.alpha = 1.0
-//                toastLabel.layer.cornerRadius = 10
-//                toastLabel.clipsToBounds = true
-//                
-//                let maxSizeTitle = CGSize(width: viewController.view.bounds.size.width - 40, height: viewController.view.bounds.size.height)
-//                var expectedSizeTitle = toastLabel.sizeThatFits(maxSizeTitle)
-//                expectedSizeTitle = CGSize(width: min(maxSizeTitle.width, expectedSizeTitle.width), height: min(maxSizeTitle.height, expectedSizeTitle.height))
-//                toastLabel.frame = CGRect(x: (viewController.view.frame.size.width - expectedSizeTitle.width) / 2,
-//                                          y: viewController.view.frame.size.height - 100,
-//                                          width: expectedSizeTitle.width + 20,
-//                                          height: expectedSizeTitle.height + 10)
-//                
-//                viewController.view.addSubview(toastLabel)
-//                
-//                UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
-//                    toastLabel.alpha = 0.0
-//                }, completion: { _ in
-//                    toastLabel.removeFromSuperview()
-//                })
-//            }
-//        }
-//}
+// MARK: - Static Func
+class ToastMessenger {
+    static func showToast(message: String, in viewController: UIViewController) {
+            DispatchQueue.main.async {
+                let toastLabel = UILabel()
+                toastLabel.text = message
+                toastLabel.textAlignment = .center
+                toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+                toastLabel.textColor = UIColor.white
+                toastLabel.alpha = 1.0
+                toastLabel.layer.cornerRadius = 10
+                toastLabel.clipsToBounds = true
+                
+                let maxSizeTitle = CGSize(width: viewController.view.bounds.size.width - 40, height: viewController.view.bounds.size.height)
+                var expectedSizeTitle = toastLabel.sizeThatFits(maxSizeTitle)
+                expectedSizeTitle = CGSize(width: min(maxSizeTitle.width, expectedSizeTitle.width), height: min(maxSizeTitle.height, expectedSizeTitle.height))
+                toastLabel.frame = CGRect(x: (viewController.view.frame.size.width - expectedSizeTitle.width) / 2,
+                                          y: viewController.view.frame.size.height - 100,
+                                          width: expectedSizeTitle.width + 20,
+                                          height: expectedSizeTitle.height + 10)
+                
+                viewController.view.addSubview(toastLabel)
+                
+                UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+                    toastLabel.alpha = 0.0
+                }, completion: { _ in
+                    toastLabel.removeFromSuperview()
+                })
+            }
+        }
+}
 
-/// 하지만 화면 전환이 이뤄지면 사라진다...
-extension UIViewController {
-    func showMessage(message: String) {
-        let messageLabel: UILabel = {
-            let label = UILabel()
-            label.textAlignment = .center
-            label.backgroundColor = .gray
-            label.textColor = .black
-            label.text = "이것도???"
-            label.layer.cornerRadius = 5
-            label.clipsToBounds = true
-            label.translatesAutoresizingMaskIntoConstraints = false
-            return label
-        }()
+// creating a toast messenger on top of window
+class Toast {
+    private var window: UIWindow? {
+        return UIApplication
+            .shared
+            .connectedScenes
+            .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }
+            .first { $0.isKeyWindow }
+    }
+    
+    func createToast(message: String) {
+        let toast = ToastMessage(message: message)
         
+        let textSize = toast.intrinsicContentSize
+        let toastWidth = UIScreen.main.bounds.width / 9 * 7
+        let labelHeight = (textSize.width / toastWidth) * 50
+        let adjustedHeight = max(labelHeight, 60)
         
+        window?.addSubview(toast)
+        NSLayoutConstraint.activate([
+            toast.topAnchor.constraint(equalTo: window?.safeAreaLayoutGuide.topAnchor ?? NSLayoutAnchor(), constant: 20),
+            toast.centerXAnchor.constraint(equalTo: window?.centerXAnchor ?? NSLayoutXAxisAnchor()),
+            toast.widthAnchor.constraint(equalToConstant: toastWidth),
+            toast.heightAnchor.constraint(equalToConstant: adjustedHeight)
+        ])
+        
+        UIView.animate(withDuration: 3.0, animations: {
+            toast.alpha = 0
+        }) { (_) in
+            toast.removeFromSuperview()
+        }
     }
 }
